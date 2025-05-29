f9ac1903e2f9de26398d2df7981ffb1af6c2f879
Syrok32 <stepstepan2@gmail.com>
2025-05-29 17:31:36 +0300

    SQL

---
+++ scripts421.sql
@@ -0,0 +1,10 @@
+ALTER TABLE student
+ADD CONSTRAINT age_constrainty CHECK (age > 16);
+
+ALTER COLUMN name SET NOT NULL,
+ADD CONSTRAINT uq_student_code UNIQUE (name);
+ALTER TABLE faculty
+ADD CONSTRAINT uq_dept_name_location UNIQUE (name, color);
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT 1;
+
