f4e72c3d8d370ec95f07f29b9b249dbeb4990d65
Syrok32 <stepstepan2@gmail.com>
2025-05-31 13:45:59 +0300

    SQL 2

--- scripts421.sql
+++ scripts421.sql
@@ -1,6 +1,6 @@
 ALTER TABLE student
 ADD CONSTRAINT age_constrainty CHECK (age > 16);
-
+ALTER TABLE student
 ALTER COLUMN name SET NOT NULL,
 ADD CONSTRAINT uq_student_code UNIQUE (name);
 ALTER TABLE faculty
