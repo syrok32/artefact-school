a74e087b8fb73dcf98f8d446fc4fb2e22576b9d8
Syrok32 <stepstepan2@gmail.com>
2025-06-04 19:14:12 +0300

    add strem and synchronized

--- src/main/java/ru/hogwarts/artefact/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/artefact/school/controller/StudentController.java
@@ -9,15 +9,24 @@ import ru.hogwarts.artefact.school.model.Student;
 import ru.hogwarts.artefact.school.services.StudentService;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("student")
 public class StudentController {
     private final StudentService studentService;
 
+
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    private synchronized void printStudentName(String name) {
+        System.out.println(name);
     }
 
     @GetMapping
@@ -75,16 +84,93 @@ public class StudentController {
         }
         return ResponseEntity.ok(faculty);
     }
+
     @GetMapping("countAllStudents")
     public Long countAllStudents() {
         return studentService.countAllStudents();
     }
+
     @GetMapping("getAverageStudentAge")
     public Double getAverageStudentAge() {
         return studentService.getAverageStudentAge();
     }
+
     @GetMapping("findLastFiveStudents")
     public List<Student> findLastFiveStudents() {
         return studentService.findLastFiveStudents();
     }
+
+    @GetMapping("filter-student")
+    public List<Student> filterStudentByA() {
+        return studentService.getAllStudents().stream().filter(student -> student.getName().startsWith("A")).collect(Collectors.toList());
+    }
+
+    @GetMapping("average-age")
+    public double getAverageAge() {
+        return studentService.getAllStudents().stream().mapToDouble(Student::getAge).average().orElse(0.0);
+    }
+
+    @GetMapping("longest-faculty")
+    public String getLongestFacultyName() {
+        return studentService.getAllStudents().stream()
+                .map(Student::getFaculty)
+                .filter(Objects::nonNull)
+                .map(Faculty::getName)
+                .max(Comparator.comparingInt(String::length))
+                .orElse("");
+    }
+
+
+    @GetMapping("calculate-sum")
+    public long calculateSum() {
+
+        return IntStream.rangeClosed(1, 1_000_000).sum();
+    }
+
+    @GetMapping("students/print-parallel")
+    public String printParallel() {
+        List<String> studentNames = studentService.getAllStudents().stream()
+                .map(Student::getName)
+                .toList();
+
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println(studentNames.get(2));
+            System.out.println(studentNames.get(3));
+        });
+
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println(studentNames.get(4));
+            System.out.println(studentNames.get(5));
+        });
+        thread1.start();
+        thread2.start();
+
+        return "ok";
+    }
+
+    @GetMapping("students/print-synchronized")
+    public String printStudentsSynchronized() {
+        List<String> studentNames = studentService.getAllStudents().stream()
+                .map(Student::getName)
+                .toList();
+
+        printStudentName(studentNames.get(0));
+        printStudentName(studentNames.get(1));
+        Thread thread1 = new Thread(() -> {
+            printStudentName(studentNames.get(2));
+            printStudentName(studentNames.get(3));
+        });
+        Thread thread2 = new Thread(() -> {
+            printStudentName(studentNames.get(4));
+            printStudentName(studentNames.get(5));
+        });
+        thread1.start();
+        thread2.start();
+
+        return "ok";
+    }
 }
+
+
